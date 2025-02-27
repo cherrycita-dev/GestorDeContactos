94bd85a5decfa025075b4a5a3d64690ffd642a0d
cherrycita-dev <pereznancyesmeralda@gmail.com>
2025-02-26 18:27:30 -0600

    Actualizar main.py
    borre funciones

--- main.py
+++ main.py
@@ -12,22 +12,7 @@ def menu():
     print("7. Guardar contactos en archivo")
     print("8. Salir")
 
-def agregar_contacto():
-    print("Agregar contacto")
-    nombre = input("Nombre: ")
-    telefono = input("Telefono: ")
-    email = input("Email: ")
 
-    contacto = {
-        "id": len(agenda) + 1,
-        "nombre": nombre,
-        "telefono": telefono,
-        "email": email
-    }
-
-    agenda.append(contacto)
-
-    print("Contacto agregado")
 
 def ver_contactos():
     print("Ver contactos")
@@ -39,21 +24,7 @@ def ver_contactos():
         print("---------------")
 
 def buscar_contacto():
-    print("Seleccione una opcion de busqueda")
-    print("1. Nombre")
-    print("2. Telefono")
-    print("3. Email")
-
-    opcion = input("Opcion: ")
-
-    if opcion == "1":
-        nombre = input("Nombre: ")
-        for contacto in agenda:
-            if contacto["nombre"] == nombre:
-                print("ID: ", contacto["id"])
-                print("Nombre: ", contacto["nombre"])
-                print("Telefono: ", contacto["telefono"])
-                print("Email: ", contacto["email"])
+  contacto["email"])
                 print("---------------")
                 encontrado = True
         if not encontrado:
@@ -113,36 +84,7 @@ def editar_contacto():
     if not encontrado:
         print("Contacto no encontrado")
 
-def eliminar_contacto():
-    ver_contactos()
-    id = int(input("ID del contacto a eliminar: "))
-    for contacto in agenda:
-        if contacto["id"] == id:
-            encontrado = True
-            agenda.remove(contacto)
-            print("Contacto eliminado")
-            break
-    if not encontrado:
-        print("Contacto no encontrado")
 
-def cargar_contactos():
-    # Encabezados de CSV o TXT: nombre, telefono, email
-
-    # Validar que el archivo exista
-    try:
-        with open("contactos.csv", "r") as archivo:
-            lineas = archivo.readlines()
-            for linea in lineas:
-                contacto = linea.strip().split(",")
-                agenda.append({
-                    "id": len(agenda) + 1,
-                    "nombre": contacto[0],
-                    "telefono": contacto[1],
-                    "email": contacto[2]
-                })
-    except FileNotFoundError:
-        print("El archivo no existe")
-        return
 
 def guardar_contactos():
     with open("contactos.csv", "w") as archivo:
